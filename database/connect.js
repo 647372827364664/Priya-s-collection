8d7dbf23701a3336b39b9be76bb7a46ee6d25627
Aryan thakur <92461885+647372827364664@users.noreply.github.com>
2024-12-08 22:58:34 +0530

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -14,3 +14,20 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/priyas-collection', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected...');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB; a 
